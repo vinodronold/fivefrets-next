ca5219c661305f1ccbb6518efd25f41c0645b74f
Vinod Ronold <vinodronoldbe@gmail.com>
2017-08-27 23:34:26 -0400

    with glamorous

--- pages/index.js
+++ pages/index.js
@@ -1,2 +1,15 @@
 import React from 'react'
-export default () => <div>PAGES</div>
+import { rehydrate, css } from 'glamor'
+import glamorous from 'glamorous'
+
+// Adds server generated styles to glamor cache.
+// Has to run before any `style()` calls
+// '__NEXT_DATA__.ids' is set in '_document.js'
+if (typeof window !== 'undefined') {
+  rehydrate(window.__NEXT_DATA__.ids)
+}
+const Wrapper = glamorous.div({
+  margin: '2rem',
+  color: 'orange'
+})
+export default () => <Wrapper>PAGES</Wrapper>
