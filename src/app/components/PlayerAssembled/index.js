d71488af6dec4bd49bfc4e809dec7e94826c856b
Vinod Ronold <vinodronoldbe@gmail.com>
2017-09-13 17:29:49 -0400

    Included Jest

--- src/app/components/PlayerAssembled/index.js
+++ src/app/components/PlayerAssembled/index.js
@@ -8,10 +8,15 @@ const Container = glamorous.div({ width: '90%', margin: '1rem auto', padding: '1
   backgroundColor: theme.color.bg()
 }))
 
-export default ({ song, player, SetTranspose }) => (
-  <Container>
-    <Headline style={{ textAlign: 'center' }}>{song.title}</Headline>
-    <Controls transpose={player.transpose} SetTranspose={SetTranspose} />
-    <DisplayChords player={player} chords={song.chords} />
-  </Container>
-)
+export default ({ song, player, SetTranspose }) => {
+  // console.log([...new Set(Object.keys(song.chords).map(i => (
+  //   `${song.chords[i].r}|${!song.chords[i].q ? 1 : song.chords[i].q}`
+  // )))])
+  return (
+    <Container>
+      <Headline style={{ textAlign: 'center' }}>{song.title}</Headline>
+      <Controls transpose={player.transpose} SetTranspose={SetTranspose} />
+      <DisplayChords player={player} chords={song.chords} />
+    </Container>
+  )
+}
--- src/app/sagas/test.js
+++
@@ -1,9 +0,0 @@
-const requests = require('./requests')
-requests
-  .getSearchResult('abcd')
-  .then(response => {
-    console.log(response.data)
-  })
-  .catch(err => {
-    console.log(err.response)
-  })
